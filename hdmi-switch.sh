1a79b8cc627559dd35fd29b5d1ecf792842fc93b
Aaron Ash <aaron.ash@gmail.com>
2017-03-19 16:48:54 +1100

    Working for disconnects.  Connects still only works when run manually

--- hdmi-switch.sh
+++ hdmi-switch.sh
@@ -5,14 +5,25 @@ export DISPLAY=:0
 #hdmi_status="$(cat /sys/class/drm/card0-HDMI-A-1/status)"
 hdmi_status="$(cat /sys/class/drm/card*-HDMI-A-1/status | head -n1)"
 
+#change-display() {
+  #local user="$(w -h | grep tty | head -n1 | awk '{print $1}')"
+
+  #if [[ $hdmi_status == "disconnected" ]]; then
+      #[[ $(pgrep X) > 0 ]] && su $user -c "xrandr --output HDMI-1 --off"
+  #else
+      ##[[ $(pgrep X) > 0 ]] && su $user -c "xrandr --output eDP-1 --auto --output HDMI-1 --auto --right-of eDP-1"
+      #[[ $(pgrep X) > 0 ]] && su $user -c "xrandr --output eDP-1 --auto --output HDMI-1 --auto --above eDP-1"
+  #fi
+#}
+
 change-display() {
   local user="$(w -h | grep tty | head -n1 | awk '{print $1}')"
-  
+  export XAUTHORITY="/home/$user/.Xauthority"
+
   if [[ $hdmi_status == "disconnected" ]]; then
-      [[ $(pgrep X) > 0 ]] && su $user -c "xrandr --output HDMI-1 --off"
+      [[ $(pgrep X) -gt 0 ]] && xrandr --output HDMI-1 --off
   else
-      #[[ $(pgrep X) > 0 ]] && su $user -c "xrandr --output eDP-1 --auto --output HDMI-1 --auto --right-of eDP-1"
-      [[ $(pgrep X) > 0 ]] && su $user -c "xrandr --output eDP-1 --auto --output HDMI-1 --auto --above eDP-1"
+      [[ $(pgrep X) -gt 0 ]] && xrandr --output eDP-1 --auto --output HDMI-1 --auto --above eDP-1
   fi
 }
 
